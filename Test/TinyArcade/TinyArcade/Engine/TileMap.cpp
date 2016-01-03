516ffa7a573d8322a8430be78814bfb765a6d219
Magnus Runesson <magnus@pokewhat.com>
2016-01-03 20:23:35 +0100

    Created new data structure for a tile map

---
+++ Test/TinyArcade/TinyArcade/Engine/TileMap.cpp
@@ -0,0 +1,9 @@
+//
+//  TileMap.cpp
+//  TinyArcade
+//
+//  Created by Magnus Runesson on 2016-01-03.
+//  Copyright © 2016 Magnus Runesson. All rights reserved.
+//
+
+#include "TileMap.h"
---
+++ Test/TinyArcade/TinyArcade/Engine/TileMap.h
@@ -0,0 +1,23 @@
+//
+//  TileMap.hpp
+//  TinyArcade
+//
+//  Created by Magnus Runesson on 2016-01-03.
+//  Copyright © 2016 Magnus Runesson. All rights reserved.
+//
+
+#ifndef TileMap_hpp
+#define TileMap_hpp
+
+#include "types.h"
+
+class CTileMap
+{
+public:
+	uint8 Width;
+	uint8 Height;
+	uint16 Padda; // Padding for alignment. Not that it matters because it will be aligned automatically, but at least I remember that this is where to put additional data
+	uint16* Tiles;
+};
+
+#endif /* TileMap_hpp */
