663b114f600c2228ec832598572cbc4ff0115343
Magnus Runesson <magnus.runesson@rovio.com>
2016-01-19 08:56:17 +0100

    Created background tile map for the space base

---
+++ Games/TinyShooter/data/tilemap_spacebase_background.h
@@ -0,0 +1,12 @@
+//
+// Data file generated from tilemap_spacebase_background.tmx
+//
+#ifndef tilemap_spacebase_background_header_
+#define tilemap_spacebase_background_header_
+
+#include "types.h"
+#include "TileMap.h"
+
+extern CTileMap tilemap_spacebase_background;
+
+#endif // tilemap_spacebase_background_header_
