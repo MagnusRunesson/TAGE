c63b9a973bf041eb0fa70ebec6d60355253941ea
Magnus Runesson <magnus.runesson@rovio.com>
2016-01-20 12:48:45 +0100

    Added animation data structure

--- TAGE/Engine/Animation.cpp
+++ TAGE/Engine/Animation.cpp
@@ -6,4 +6,28 @@
 //  Copyright (c) 2016 Magnus Runesson. All rights reserved.
 //
 
-#include "ImageAnimation.h"
+#include "GameObject.h"
+#include "Animation.h"
+
+void Animation::Create( AnimationSequenceDefinition* _pSequence, GameObject* _pTarget )
+{
+	pTarget = _pTarget;
+	pSequence = _pSequence;
+	FrameIndex = 0;
+	FrameTime = 0;
+}
+
+void Animation::Update()
+{
+	FrameTime++;
+	int duration = pSequence->Frames[ FrameIndex ].Duration;
+	if( FrameTime >= duration )
+	{
+		FrameIndex++;
+		if( FrameIndex >= pSequence->NumFrames )
+			FrameIndex -= pSequence->NumFrames;
+		FrameTime -= duration;
+		
+		pTarget->GetSprite()->image = pSequence->Frames[ FrameIndex ].sourceImage;
+	}
+}
--- TAGE/Engine/Animation.h
+++ TAGE/Engine/Animation.h
@@ -9,6 +9,39 @@
 #ifndef __TinyShooter__ImageAnimation__
 #define __TinyShooter__ImageAnimation__
 
-#include <stdio.h>
+#include "types.h"
+
+class GameObject;
+class Image;
+
+class AnimationFrameDefinition
+{
+public:
+	Image* sourceImage;
+	uint8 Duration;
+	uint8 Flags;
+	sint8 HotspotX;
+	sint8 HotspotY;
+};
+
+class AnimationSequenceDefinition
+{
+public:
+	int NumFrames;
+	AnimationFrameDefinition* Frames;
+};
+
+class Animation
+{
+public:
+	GameObject* pTarget;
+	AnimationSequenceDefinition* pSequence;
+	int FrameIndex;
+	int FrameTime;
+
+	void Create( AnimationSequenceDefinition* _pSequence, GameObject* _pTarget );
+	void Update();
+};
+
 
 #endif /* defined(__TinyShooter__ImageAnimation__) */
