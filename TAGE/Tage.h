0d267c721619d542314ff3dcf4857efebb5fe8a7
Magnus Runesson <magnus@pokewhat.com>
2016-01-26 17:16:19 +0100

    Added main include file that the main sketch can use.

---
+++ TAGE/Tage.h
@@ -0,0 +1,41 @@
+//
+//  TAGE.h
+//  
+//
+//  Created by Magnus Runesson on 2014-11-24.
+//
+//
+
+#ifndef __TAGE_h
+#define __TAGE_h
+
+// Audio
+#include "Engine/Audio/AudioData.h"
+#include "Engine/Audio/AudioMixer.h"
+#include "Engine/Audio/AudioSource.h"
+
+// Graphics
+#include "Engine/Graphics/Animation.h"
+#include "Engine/Graphics/Image.h"
+#include "Engine/Graphics/Screen.h"
+#include "Engine/Graphics/Sprite.h"
+#include "Engine/Graphics/SpriteRenderer.h"
+#include "Engine/Graphics/TileBank.h"
+#include "Engine/Graphics/TileMap.h"
+#include "Engine/Graphics/TileRenderer.h"
+
+// Math
+#include "Engine/Math/FixedPoint.h"
+#include "Engine/Math/fp2d.h"
+#include "Engine/Math/fpmath.h"
+
+// Scene
+#include "Engine/Scene/Camera.h"
+#include "Engine/Scene/GameObject.h"
+#include "Engine/Scene/GameObjectManager.h"
+#include "Engine/Scene/GameObjectTemplate.h"
+
+// 
+#include "Engine/BitHelpers.h"
+
+#endif
