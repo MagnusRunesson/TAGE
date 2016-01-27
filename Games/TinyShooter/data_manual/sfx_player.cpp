e2029995d3f21677a97a5013f3ffa0e1af863511
Magnus Runesson <magnus@pokewhat.com>
2016-01-27 20:24:00 +0100

    Fixed a type mismatch for the audio samples used by the sound effects

--- Games/TinyShooter/data_manual/sfx_player.cpp
+++ Games/TinyShooter/data_manual/sfx_player.cpp
@@ -9,19 +9,19 @@
 #include "data_manual/sfx_player.h"
 #include "Engine/Audio/AudioData.h"
 
-extern "C" const sint8 audiosamples_pew_s8b_pcm_11025hz[];
+extern "C" const unsigned char audiosamples_pew_s8b_pcm_11025hz[];
 const AudioData sfx_player_fire_canon =
 {
 	1320,
 	false,
-	audiosamples_pew_s8b_pcm_11025hz,
+	(const sint8*)audiosamples_pew_s8b_pcm_11025hz,
 };
 
 
-extern "C" const sint8 audiosamples_pickup_s8b_pcm_11025[];
+extern "C" const unsigned char audiosamples_pickup_s8b_pcm_11025[];
 const AudioData sfx_player_pickup =
 {
 	8368,
 	false,
-	audiosamples_pickup_s8b_pcm_11025,
+	(const sint8*)audiosamples_pickup_s8b_pcm_11025,
 };
