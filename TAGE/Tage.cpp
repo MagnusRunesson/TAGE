79d54ace9e4c5247d3e72a7e7429a49dddccf603
Magnus Runesson <magnus@pokewhat.com>
2016-01-27 20:25:12 +0100

    Since I don't know how to get Arduino to include multiple source file from a library I've simply created a library file which in turn will include all the source files for the project.

---
+++ TAGE/Tage.cpp
@@ -0,0 +1,29 @@
+//
+//  Tage.cpp
+//  TAGE
+//
+//  Created by Magnus Runesson on 2016-01-27.
+//  Copyright © 2016 Magnus Runesson. All rights reserved.
+//
+
+#include "Engine/Audio/AudioData.cpp"
+#include "Engine/Audio/AudioMixer.cpp"
+#include "Engine/Audio/AudioSource.cpp"
+#include "Engine/BitHelpers.cpp"
+#include "Engine/Graphics/Animation.cpp"
+#include "Engine/Graphics/Image.cpp"
+#include "Engine/Graphics/Screen.cpp"
+#include "Engine/Graphics/Sprite.cpp"
+#include "Engine/Graphics/SpriteRenderer.cpp"
+#include "Engine/Graphics/TileBank.cpp"
+#include "Engine/Graphics/TileMap.cpp"
+#include "Engine/Graphics/TileRenderer.cpp"
+#include "Engine/Math/FixedPoint.cpp"
+#include "Engine/Math/fp2d.cpp"
+#include "Engine/Math/fpmath.cpp"
+#include "Engine/Math/sinetable.cpp"
+#include "Engine/Scene/Camera.cpp"
+#include "Engine/Scene/GameObject.cpp"
+#include "Engine/Scene/GameObjectManager.cpp"
+#include "Engine/Scene/GameObjectTemplate.cpp"
+#include "Abstraction Layer/Joypad.cpp"
