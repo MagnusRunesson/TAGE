d49aad9a634d6eb665ab5dbda4b819fec5d20b3f
Magnus Runesson <magnus.runesson@rovio.com>
2016-01-29 13:00:43 +0100

    Added functionality to get squared distance between two fp2d, because that is cool!

--- TAGE/Engine/Math/fpmath.cpp
+++ TAGE/Engine/Math/fpmath.cpp
@@ -6,9 +6,11 @@
 //  Copyright © 2015 Magnus Runesson. All rights reserved.
 //
 
+#include "Engine/Types.h"
 #include "Engine/Math/fpmath.h"
 #include "Engine/Math/sinetable.h"
-
+#include "Engine/Math/FixedPoint.h"
+#include "Engine/Math/fp2d.h"
 
 sint16 fpsin( int _angle )
 {
@@ -19,4 +21,13 @@ sint16 fpsin( int _angle )
 sint16 fpcos( int _angle )
 {
 	return fpsin( _angle + 64 );
+}
+
+FixedPoint SqrDistance( const fp2d& _a, const fp2d& _b )
+{
+	FixedPoint x = _b.x - _a.x;
+	FixedPoint y = _b.y - _a.y;
+	
+	FixedPoint ret = (x*x)+(y*y);
+	return ret;
 }
--- TAGE/Engine/Math/fpmath.h
+++ TAGE/Engine/Math/fpmath.h
@@ -10,8 +10,12 @@
 #define fpmath_hpp
 
 #include "Engine/Types.h"
+#include "Engine/Math/FixedPoint.h"
+#include "Engine/Math/fp2d.h"
 
 sint16 fpsin( int _angle );
 sint16 fpcos( int _angle );
+
+FixedPoint SqrDistance( const fp2d& _a, const fp2d& _b );
 
 #endif /* fpmath_hpp */
