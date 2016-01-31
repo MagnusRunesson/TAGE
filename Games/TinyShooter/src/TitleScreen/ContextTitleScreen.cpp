a9b5cebcb9f0d16decca2b9cae41c504cd265771
Magnus Runesson <magnus@pokewhat.com>
2016-01-31 16:09:07 +0100

    Added the Tiny Shooter text to the title screen

--- Games/TinyShooter/src/TitleScreen/ContextTitleScreen.cpp
+++ Games/TinyShooter/src/TitleScreen/ContextTitleScreen.cpp
@@ -32,6 +32,9 @@ extern AudioSource* bgm;
 
 extern bool debugSpriteRenderer;
 
+Sprite* titleSprites[ 4 + 7 ];	// TINY=4 letters, SHOOTER=7 letters
+int letterIndex;
+
 void(*pfnHBlankInterruptTitleScreen)(int);
 
 void HBlankInterruptTitleScreen( int _scanline )
@@ -55,6 +58,17 @@ void HBlankInterruptTitleScreen( int _scanline )
 	//printf( "Scanline=%i\n", _scanline );
 }
 
+void setupLetter( const Image* _pImage, int _x, int _y )
+{
+	Sprite* pSprite = spriteRenderer.AllocateSprite( _pImage );
+
+	pSprite->x = _x;
+	pSprite->y = _y;
+	
+	titleSprites[ letterIndex ] = pSprite;
+	letterIndex++;
+}
+
 void titlescreen_setup()
 {
 	pfnHBlankInterruptTitleScreen = HBlankInterruptTitleScreen;
@@ -63,6 +77,28 @@ void titlescreen_setup()
 	gameObjectManager.Reboot();
 	spriteRenderer.Reboot();
 	audioMixer.Reboot();
+	
+	// Setup the letters
+	int x, y;
+	letterIndex = 0;
+	
+	x = 20;
+	y = 10;
+	setupLetter( &sprite_logo_tiny_t, x, y ); x += sprite_logo_tiny_t.w+1;
+	setupLetter( &sprite_logo_tiny_i, x, y ); x += sprite_logo_tiny_i.w+1;
+	setupLetter( &sprite_logo_tiny_n, x, y ); x += sprite_logo_tiny_n.w+1;
+	setupLetter( &sprite_logo_tiny_y, x, y ); x += sprite_logo_tiny_y.w+1;
+
+	x = 10;
+	y = 30;
+	setupLetter( &sprite_logo_shooter_s, x, y ); x += sprite_logo_shooter_s.w+1;
+	setupLetter( &sprite_logo_shooter_h, x, y ); x += sprite_logo_shooter_h.w+1;
+	setupLetter( &sprite_logo_shooter_o, x, y ); x += sprite_logo_shooter_o.w+1;
+	setupLetter( &sprite_logo_shooter_o, x, y ); x += sprite_logo_shooter_o.w+1;
+	setupLetter( &sprite_logo_shooter_t, x, y ); x += sprite_logo_shooter_t.w+1;
+	setupLetter( &sprite_logo_shooter_e, x, y ); x += sprite_logo_shooter_e.w+1;
+	setupLetter( &sprite_logo_shooter_r, x, y ); x += sprite_logo_shooter_r.w+1;
+	
 }
 
 void titlescreen_loop()
