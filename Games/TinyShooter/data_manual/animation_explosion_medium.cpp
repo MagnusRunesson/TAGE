c797abacecad2c190c3cf2621fbc7311926c0853
Magnus Runesson <magnus@pokewhat.com>
2016-02-17 13:05:31 +0100

    Changed alignment of the explosion animation, so it better fit with where the explosion is spawned.

--- Games/TinyShooter/data_manual/animation_explosion_medium.cpp
+++ Games/TinyShooter/data_manual/animation_explosion_medium.cpp
@@ -10,70 +10,73 @@
 #include "Engine/Graphics/Animation.h"
 #include "data/alldata.h"
 
+#define AEM_HOTSPOT_X (5)
+#define AEM_HOTSPOT_Y (5)
+
 const AnimationFrameDefinition animation_explosion_medium_frames[] =
 {
 	{
 		&sprite_explosion_medium_00,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_01,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_02,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_03,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_04,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_05,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_06,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_07,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 	{
 		&sprite_explosion_medium_08,
 		5,
 		0,
-		0,
-		0,
+		AEM_HOTSPOT_X,
+		AEM_HOTSPOT_Y,
 	},
 };
 
