7b4db05a6abf87057349cc603fa446b7cfdd618c
Magnus Runesson <magnus@pokewhat.com>
2016-02-19 13:05:02 +0100

    Added converted music to project

---
+++ Games/TinyShooter/data/audiosamples_music_titlescreen_s8b_pcm_32000hz.h
@@ -0,0 +1,11 @@
+//
+// Generated from source file 'audiosamples_music_titlescreen_s8b_pcm_32000hz.raw'
+//
+
+#ifndef __audiosamples_music_titlescreen_s8b_pcm_32000hz_h
+#define __audiosamples_music_titlescreen_s8b_pcm_32000hz_h
+
+extern "C" const unsigned char audiosamples_music_titlescreen_s8b_pcm_32000hz[];
+
+#endif // __audiosamples_music_titlescreen_s8b_pcm_32000hz_
+
