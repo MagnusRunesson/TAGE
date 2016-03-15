70d37785ac483ee7f19bb2105e429706b3e58d04
Magnus Runesson <magnus@pokewhat.com>
2016-03-15 07:25:33 +0100

    Added 11025 Hz title screen music, even though the c++/h files aren't really used. On the actual hardware the music is streamed from memory card, and on Mac it doesn't play at all. But still. :)

---
+++ Games/TinyShooter/data/audiosamples_music_titlescreen_s8b_pcm_11025hz.h
@@ -0,0 +1,11 @@
+//
+// Generated from source file 'audiosamples_music_titlescreen_s8b_pcm_11025hz.raw'
+//
+
+#ifndef __audiosamples_music_titlescreen_s8b_pcm_11025hz_h
+#define __audiosamples_music_titlescreen_s8b_pcm_11025hz_h
+
+extern "C" const unsigned char audiosamples_music_titlescreen_s8b_pcm_11025hz[];
+
+#endif // __audiosamples_music_titlescreen_s8b_pcm_11025hz_
+
