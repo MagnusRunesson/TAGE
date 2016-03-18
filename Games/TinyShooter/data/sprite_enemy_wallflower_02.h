dca9f3e63884cea6d5e2f8cc04ac8860e03265aa
Magnus Runesson <magnus@pokewhat.com>
2016-03-18 07:14:45 +0100

    Create cpp/h files of the wallflower sprites

---
+++ Games/TinyShooter/data/sprite_enemy_wallflower_02.h
@@ -0,0 +1,6 @@
+#ifndef sprite_enemy_wallflower_02_data_h
+#define sprite_enemy_wallflower_02_data_h
+
+extern "C" const Image sprite_enemy_wallflower_02;
+
+#endif // sprite_enemy_wallflower_02_data_h
