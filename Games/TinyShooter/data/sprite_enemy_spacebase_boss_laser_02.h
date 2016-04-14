c824d2282fb596dea5aec17c3d7f6e0679bda1d8
Magnus Runesson <magnus@pokewhat.com>
2016-04-14 10:11:19 +0200

    Added exported cpp/h files for spacebase boss laser

---
+++ Games/TinyShooter/data/sprite_enemy_spacebase_boss_laser_02.h
@@ -0,0 +1,6 @@
+#ifndef sprite_enemy_spacebase_boss_laser_02_data_h
+#define sprite_enemy_spacebase_boss_laser_02_data_h
+
+extern "C" const Image sprite_enemy_spacebase_boss_laser_02;
+
+#endif // sprite_enemy_spacebase_boss_laser_02_data_h
---
+++ Games/TinyShooter/data/sprite_enemy_spacebase_boss_laser_03.h
@@ -0,0 +1,6 @@
+#ifndef sprite_enemy_spacebase_boss_laser_03_data_h
+#define sprite_enemy_spacebase_boss_laser_03_data_h
+
+extern "C" const Image sprite_enemy_spacebase_boss_laser_03;
+
+#endif // sprite_enemy_spacebase_boss_laser_03_data_h
