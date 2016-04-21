1479260f02d9a205a8efc74932274d5f7508960f
Magnus Runesson <magnus@pokewhat.com>
2016-04-21 17:44:37 +0200

    - Removed a lot of unused code in the win screen. - Made it possible to go from the win screen to the title screen. We now have a full game loop, yay!

--- Games/TinyShooter/src/WinScreen/ContextWinScreen.cpp
+++ Games/TinyShooter/src/WinScreen/ContextWinScreen.cpp
@@ -29,84 +29,25 @@
 #include "data/alldata.h"
 #include "src/ContextManager.h"
 
-/*
-#define TITLESCREEN_CLOSETIMER_TOTALDURATION (60)
-#define TITLESCREEN_CLOSETIMER_BLINKSHIFT (3)
-#define TITLESCREEN_CLOSETIMER_SFXSTOP (TITLESCREEN_CLOSETIMER_TOTALDURATION-5)
-#define TITLESCREEN_CREDITSTIMER_TOTAL (50)
-#define TITLESCREEN_CREDITSTIMER_INVISBLE (30)
-#define TITLESCREEN_CREDITSHEIGHT (45)
+//
+#define WINSCREEN_CLOSETIMER_DURATION (60)
+#define WINSCREEN_CLOSETIMER_SFXSTOP (WINSCREEN_CLOSETIMER_DURATION-5)
 
-#define TITLESCREEN_FLASHTIMER_DURATION_REPEAT (240)
-#define TITLESCREEN_FLASHTIMER_DURATION_INITIAL (60)
-#define TITLESCREEN_FLASHTIMER_LETTER_DURATION (6)
-#define TITLESCREEN_FLASHTIMER_START (56>>3)
-
-const int titleSpriteFlashDelay[ 4+7 ] =
-{
-	43, 28, 21, 8,				// 8, 21, 28, 43,
-	56, 48, 39, 29, 19, 9, 0,	// 0, 9, 19, 29, 39, 48, 56,
-};
-*/
-
+// Just reuse as many variables as possible from the title screen as to not use more RAM than necessary. Consider this old school overlays. :)
 extern TinyScreen display;
 extern uint16* lineBuffer;
-
-
-//
-//extern AudioSource* bgm;
-//AudioSource* sfxPressStart;
-
 extern void debugPrintSystems();
-
 extern bool debugSpriteRenderer;
-
-/*
-Sprite* titleSprites[ 4 + 7 ];	// TINY=4 letters, SHOOTER=7 letters
-Sprite* titleScreenInsertCredits;
-Sprite* titleScreenWinners;
-int titlescreenCloseTimer;
-int titleScreenTimer;
-int titleScreenFlashTimer;
- */
-
 extern uint8 collisionBits[];
 extern uint8 collisionIndices[];
+extern AudioSource* sfxPressStart;
+extern int titlescreenCloseTimer;
 
 void(*pfnHBlankInterruptWinScreen)(int);
 
 void HBlankInterruptWinScreen( int _scanline )
 {
-	/*
-	if( _scanline < 5 )
-	{
-		int x, y;
-		background->GetPosition( &x, &y );
-		x--;
-		background->SetPosition( x, y );
-	} else if( _scanline > 59 )
-	{
-		int x, y;
-		background->GetPosition( &x, &y );
-		x++;
-		background->SetPosition( x, y );
-	}
-	 */
-	
-	//printf( "Scanline=%i\n", _scanline );
 }
-
-/*
-void setupLetter( int _index, const Image* _pImage, int _x, int _y )
-{
-	Sprite* pSprite = spriteRenderer.AllocateSprite( _pImage );
-
-	pSprite->x = _x;
-	pSprite->y = _y;
-	
-	titleSprites[ _index ] = pSprite;
-}
-*/
 
 void winscreen_setup()
 {
@@ -117,39 +58,6 @@ void winscreen_setup()
 	spriteRenderer.Reboot();
 	audioMixer.Reboot();
 
-	/*
-	// Setup the letters
-	int x, y;
-	
-	x = 23;
-	y = 6;
-	setupLetter( 0, &sprite_logo_tiny_t, x, y ); x += sprite_logo_tiny_t.w+1;
-	setupLetter( 1, &sprite_logo_tiny_i, x, y ); x += sprite_logo_tiny_i.w+1;
-	setupLetter( 2, &sprite_logo_tiny_n, x, y ); x += sprite_logo_tiny_n.w+1;
-	setupLetter( 3, &sprite_logo_tiny_y, x, y ); x += sprite_logo_tiny_y.w+1;
-
-	x = 15;
-	y = 20;
-	setupLetter(  4, &sprite_logo_shooter_s, x, y ); x += sprite_logo_shooter_s.w+1;
-	setupLetter(  5, &sprite_logo_shooter_h, x, y ); x += sprite_logo_shooter_h.w+1;
-	setupLetter(  6, &sprite_logo_shooter_o, x, y ); x += sprite_logo_shooter_o.w+1;
-	setupLetter(  7, &sprite_logo_shooter_o, x, y ); x += sprite_logo_shooter_o.w+1;
-	setupLetter(  8, &sprite_logo_shooter_t, x, y ); x += sprite_logo_shooter_t.w+1;
-	setupLetter(  9, &sprite_logo_shooter_e, x, y ); x += sprite_logo_shooter_e.w+1;
-	setupLetter( 10, &sprite_logo_shooter_r, x, y ); x += sprite_logo_shooter_r.w+1;
-	
-	titleScreenInsertCredits = spriteRenderer.AllocateSprite( &sprite_titlescreen_insertcredits );
-	titleScreenInsertCredits->x = 21;
-	titleScreenInsertCredits->y = TITLESCREEN_CREDITSHEIGHT;
-	
-	titleScreenWinners = spriteRenderer.AllocateSprite( &sprite_titlescreen_winnersdontusedrugs );
-	titleScreenWinners->x = 7;
-	titleScreenWinners->y = 58;
-	
-	// Cool background music
-	//bgm = audioMixer.GetChannel( 0 );
-	//bgm->SetData( &music_titlescreen );
-	//bgm->PlayFromBeginning();
 	
 	// Sound effect for when the player decides to start the game
 	sfxPressStart = audioMixer.GetChannel( 1 );
@@ -160,10 +68,7 @@ void winscreen_setup()
 	pStream->OpenStream( "bgm_title.raw" );
 	pStream->Play();
 
-	//
 	titlescreenCloseTimer = 0;
-	titleScreenFlashTimer = TITLESCREEN_FLASHTIMER_DURATION_INITIAL;
-	 */
 }
 
 void winscreen_loop()
@@ -175,77 +80,33 @@ void winscreen_loop()
 	//
 	padUpdate();
 	
-	/*
-	//
-	titleScreenFlashTimer--;
-	if( titleScreenFlashTimer < TITLESCREEN_FLASHTIMER_START )
-	{
-		int i;
-		for( i=0; i<4+7; i++ )
-		{
-			int offset = titleScreenFlashTimer - (titleSpriteFlashDelay[ i ] >> 3);
-			if( offset >= 0 )
-				titleSprites[ i ]->ClrFlags( SPRITE_FLAG_DRAWWHITE );
-			else if( offset <= -TITLESCREEN_FLASHTIMER_LETTER_DURATION )
-				titleSprites[ i ]->ClrFlags( SPRITE_FLAG_DRAWWHITE );
-			else
-				titleSprites[ i ]->SetFlags( SPRITE_FLAG_DRAWWHITE );
-		}
-		
-		if( titleScreenFlashTimer == -TITLESCREEN_FLASHTIMER_START )
-			titleScreenFlashTimer = TITLESCREEN_FLASHTIMER_DURATION_REPEAT;
-	}
-
 	//
 	if( titlescreenCloseTimer == 0 )
 	{
-		titleScreenTimer--;
-		if( titleScreenTimer <= 0 )
-			titleScreenTimer += TITLESCREEN_CREDITSTIMER_TOTAL;
-		
-		if( titleScreenTimer > TITLESCREEN_CREDITSTIMER_INVISBLE )
-			titleScreenInsertCredits->y = -10;
-		else
-			titleScreenInsertCredits->y = TITLESCREEN_CREDITSHEIGHT;
-		
 		// This is regular code, before the user press Start
 		if( padGetPressed() & PAD_KEYMASK_PRIMARY )
 		{
 			audioMixer.GetStream( 0 )->Pause();
-			//audioMixer.SetFrequency( 11025 );
-			titlescreenCloseTimer = TITLESCREEN_CLOSETIMER_TOTALDURATION;
-			if( titleScreenFlashTimer > TITLESCREEN_FLASHTIMER_START )
-				titleScreenFlashTimer = TITLESCREEN_FLASHTIMER_START;
+			titlescreenCloseTimer = WINSCREEN_CLOSETIMER_DURATION;
 		}
-	}
-	else
+	} else
 	{
-		// User have pressed start and we're just waiting
 		titlescreenCloseTimer--;
-
-		// Flashing text
-		if((titlescreenCloseTimer>>TITLESCREEN_CLOSETIMER_BLINKSHIFT) & 1 )
-			titleScreenInsertCredits->y = -10;
-		else
-			titleScreenInsertCredits->y = TITLESCREEN_CREDITSHEIGHT;
 		
 		// Play the "confirm" audio. If it starts on the same frame as we stop the
 		// bgm music the sound effect isn't heard, it just blends into the music.
 		// So that's why there is a slight delay before playing the sound.
-		if( titlescreenCloseTimer == TITLESCREEN_CLOSETIMER_SFXSTOP )
+		if( titlescreenCloseTimer == WINSCREEN_CLOSETIMER_SFXSTOP )
 			sfxPressStart->PlayFromBeginning();
 
 		// Timer has run out. Start the game
 		if( titlescreenCloseTimer == 0 )
-			contextGotoIngame();
+			contextGotoTitleScreen();
 	}
-	*/
 	
 	//
 	// Scanline rendered
 	//
-	//uint16* screen = screenBuffer;
-
 	spriteRenderer.FrameStart();
 
 	display.setX( 0, SCREEN_WIDTH );
@@ -270,7 +131,6 @@ void winscreen_loop()
 			lineBuffer[ x ] = rgb;
 		}
 		
-		//	spriteRenderer.RenderPixel( x, &rgb, &spriteCollisionMask );
 		spriteRenderer.RenderScanline( lineBuffer, collisionBits, collisionIndices );
 			
 #ifdef TAGE_TARGET_MACOSX
@@ -286,11 +146,6 @@ void winscreen_loop()
 		while( !display.getReadyStatusDMA());
 #endif
 		
-		/*
-		// Copy from line buffer to "hardware" screen
-		for( x=0; x<SCREEN_WIDTH; x++ )
-			*screen++ = lineBuffer[ x ];*/
-
 		spriteRenderer.NextScanline( debugSpriteRenderer );
 		
 		iScanline++;
